7eb80f285152c469de38720d088ca48b9d7deb26
harryg15 <harry.grant99@googlemail.com>
2023-02-24 15:17:15 +0000

    further formatted users.js

--- routes/users.js
+++ routes/users.js
@@ -31,25 +31,25 @@ check("title").isEmpty().trim(),
 check("genre").isEmpty().trim(),
 check("rating").isEmpty().trim(),
 check("status").not().isEmpty().trim().matches("watched")
-.withMessage("Must enter status 'watched'")
+.withMessage("Must enter status: 'watched'")
 
 ], async (req, res) => {
 
     const user = await User.findByPk(req.params.id)
     const userShows = await user.getShows()
-    const showToUpdate = await Show.findByPk(req.params.show)
 
-    const min = userShows[0].dataValues.id;
-    const max = userShows[userShows.length - 1].dataValues.id
+    const min = userShows[0].dataValues.id; //first associated show with 'user'
+    const max = userShows[userShows.length - 1].dataValues.id //last associated show with 'user'
 
     const errors = validationResult(req)
-    
+
     if(!errors.isEmpty()) {
         res.send({error: errors.array()})
     } else if (req.params.show < min | req.params.show > max) {
         // if show < 4 OR show > 6 (user2 having related shows in this case with 'id' 4-6, then...)
         res.send({error: `User doesn't have this show in their collection. Pick an 'id' between ${min} & ${max}`})
     } else {
+        const showToUpdate = await Show.findByPk(req.params.show)
         const newStatus = {status: req.body.status}
         await showToUpdate.update(newStatus)
         res.json(await user.getShows())
